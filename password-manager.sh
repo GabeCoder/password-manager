0dfb67700073d8bfad8d1af39245169f17015e74
GabeTheCoder <72320038+GabeCoder@users.noreply.github.com>
2021-03-25 16:41:32 -0400

    Add files via upload

---
+++ password-manager.sh
@@ -0,0 +1,45 @@
+#!/bin/bash
+
+homename=$(basename "$HOME")
+homescreen() {
+	cd "$HOME"/password-manager
+	yad --window-icon=icon.png --image=icon.png --title="Password Manager" --text="Welcome, $homename. What do you want to do?" --button="Add Password":10 --button="Browse Passwords":20 --width=300 --height=300
+	answer=$?
+	cd "$HOME"/.passwords
+	if [ $answer == 10 ]
+	then
+		username=$(zenity --entry --title="Username" --text="Type your username" --width=300)
+		username="${username// /_}"
+		password=$(zenity --password --title="Password" --width=300)
+		echo "$password" > "$username"
+		homescreen &
+		sleep 1
+		zenity --info --icon-name=emblem-default --title="Success" --text="$username's password has been added." --width=300
+	elif [ $answer == 20 ]
+	then
+		list() {
+			list=$(ls -Q)
+			echo $list
+			username=$(yad --list --title="Passwords" --window-icon="$HOME"/password-manager/icon.png --no-headers --column="Username" ${list//'"'} --button="Back":10 --button="Reveal Password":20 --button="Delete Password":30 --height=300)
+			answer=$?
+			if [ $answer == 10 ]
+			then
+				homescreen
+			elif [ $answer == 20 ]
+			then
+				password=$(cat "${username//|}")
+				list &
+				sleep 1
+				zenity --info --title="Password" --text="$password" --width=300 &
+			elif [ $answer == 30 ]
+			then
+				unlink "${username//|}"
+				list &
+				sleep 1
+				zenity --info --icon-name=emblem-default --title="Success" --text="${username//|}'s password has been deleted." --width=300
+			fi
+		}
+		list
+	fi
+}
+homescreen
